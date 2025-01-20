772e38a3dba4a3527e0a6baf8e6112a07a3eddb2
AngryL1on <vadim.angryl1on@gmail.com>
2025-01-20 23:18:27 +0300

    Refactor code in RabbitMQListener class

--- src/main/java/dev/angryl1on/library/core/RabbitMQListeners.java
+++ src/main/java/dev/angryl1on/library/core/RabbitMQListeners.java
@@ -1,8 +1,6 @@
 package dev.angryl1on.library.core;
 
-import dev.angryl1on.library.core.configs.RabbitMQConfig;
 import dev.angryl1on.library.core.services.NotificationService;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,16 +11,6 @@ public class RabbitMQListeners {
     @Autowired
     public RabbitMQListeners(NotificationService notificationService) {
         this.notificationService = notificationService;
-    }
-
-    @RabbitListener(queues = RabbitMQConfig.NEW_BOOKS_QUEUE)
-    public void handleNewBookNotification(String message) {
-        notificationService.sendNotification("/topic/new-books", message);
-    }
-
-    @RabbitListener(queues = RabbitMQConfig.NEW_LIBRARIES_QUEUE)
-    public void handleNewLibraryNotification(String message) {
-        notificationService.sendNotification("/topic/new-libraries", message);
     }
 }
 
